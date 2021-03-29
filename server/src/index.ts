28aa39a5d2945617b6e5fa7afa0dc49d74a6cc1c
Omar Adel <48330931+omarxadel@users.noreply.github.com>
2021-03-29 15:54:16 +0200

    Update index.ts
    Connect apollo server to resolvers, create typeorm connection and conect to express server

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,21 +1,46 @@
 import "reflect-metadata";
-import {createConnection} from "typeorm";
-import {User} from "./entity/User";
+import express from 'express';
+import {ApolloServer} from 'apollo-server-express';
+import { buildSchema } from "type-graphql";
+import { UserResolver } from "./UserResolver";
+import { createConnection } from "typeorm";
 
-createConnection().then(async connection => {
+(async () => {
+    const app = express();
+    app.get('/', (_req, res) => res.send('hello'));
 
-    console.log("Inserting a new user into the database...");
-    const user = new User();
-    user.firstName = "Timber";
-    user.lastName = "Saw";
-    user.age = 25;
-    await connection.manager.save(user);
-    console.log("Saved a new user with id: " + user.id);
+    await createConnection();
 
-    console.log("Loading users from the database...");
-    const users = await connection.manager.find(User);
-    console.log("Loaded users: ", users);
+    const apolloServer = new ApolloServer({
+        schema: await buildSchema({
+            resolvers: [UserResolver]
+        }),
+        context: ({ req, res }) => ({ req, res })
+    });
 
-    console.log("Here you can setup and run express/koa/any other framework.");
+    apolloServer.applyMiddleware({ app });
 
-}).catch(error => console.log(error));
+    app.listen(4000, () =>{
+        console.log("express server started");
+        
+    });
+})();
+
+
+// createConnection().then(async connection => {
+
+//     console.log("Inserting a new user into the database...");
+//     const user = new User();
+//     user.firstName = "Timber";
+//     user.lastName = "Saw";
+//     user.age = 25;
+//     await connection.manager.save(user);
+//     console.log("Saved a new user with id: " + user.id);
+
+//     console.log("Loading users from the database...");
+//     const users = await connection.manager.find(User);
+//     console.log("Loaded users: ", users);
+
+//     console.log("Here you can setup and run express/koa/any other framework.");
+
+// }).catch(error => console.log(error));
